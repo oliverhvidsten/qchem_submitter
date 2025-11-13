51d78793fd5e05eb2a17b08d68ed01defa7bd134
oliverhvidsten <oliverhvidsten@gmail.com>
2025-11-13 15:05:36 -0800

    implement for lrc

--- create_optimization_inputs.py
+++ create_optimization_inputs.py
@@ -1,22 +1,21 @@
 from pymatgen.io.qchem.inputs import QCInput
 from pymatgen.core.structure import Molecule
 
-import os
-import json
+import os, sys, json
 
 
-def main():
+def main(system_name):
     cwd = os.getcwd()
     mols = gather_molecules("./init_mols")
 
     for molecule_name, molecule_obj in mols.items():
-        create_inputs(molecule_name, molecule_obj)
+        create_inputs(molecule_name, molecule_obj, system_name)
         os.chdir(cwd)
         
 
 
 
-def create_inputs(name, mol_obj):
+def create_inputs(name, mol_obj, system_name):
     """ Creates Q-Chem Input files for a molecule"""
     os.makedirs(f"./outputs/{name}")
     os.chdir(f"./outputs/{name}")
@@ -29,33 +28,56 @@ def create_inputs(name, mol_obj):
     )
 
     qcin.write_file(f"{name}.qin")
-    script = submit_script(name)
+    script = submit_script(name, system_name)
 
     with open("submit.script", "w") as f:
         f.write(script)
 
 
-def submit_script(name):
+def submit_script(name, system_name):
     """ Creates a submit script to submit the jobs to the cluster """
-    lines = [
-        "#!/bin/bash -l",
-        "",
-        "#SBATCH --nodes=1",
-        "#SBATCH --qos=normal",
-        "#SBATCH --time=24:00:00",
-        "#SBATCH --partition=standard",
-        "#SBATCH --account=silimorphous",
-        "#SBATCH --job-name=geom_opt",
-        "#SBATCH --output=FW_job-%j.out",
-        "#SBATCH --error=FW_job-%j.error",
-        "#SBATCH --ntasks=128",
-        "",
-        "source activate cms_atomate",
-        "module load gcc",
-        "module load q-chem/6.0",
-        "",
-        f"qchem {name}.qin {name}.qout"
-    ]
+    
+    if system_name == "swift":
+    
+        lines = [
+            "#!/bin/bash -l",
+            "",
+            "#SBATCH --nodes=1",
+            "#SBATCH --qos=normal",
+            "#SBATCH --time=24:00:00",
+            "#SBATCH --partition=standard",
+            "#SBATCH --account=silimorphous",
+            "#SBATCH --job-name=geom_opt",
+            "#SBATCH --output=FW_job-%j.out",
+            "#SBATCH --error=FW_job-%j.error",
+            "#SBATCH --ntasks=128",
+            "",
+            "source activate cms_atomate",
+            "module load gcc",
+            "module load q-chem/6.0",
+            "",
+            f"qchem {name}.qin {name}.qout"
+        ]
+
+    elif system_name == "lrc":
+
+        lines = [
+            "#!/bin/bash -l",
+            "",
+            "#SBATCH --nodes=1",
+            "#SBATCH --qos=condo_mp_lr6",
+            "#SBATCH --partition=lr6",
+            "#SBATCH --account=silimorphous",
+            "#SBATCH --job-name=geom_opt",
+            "#SBATCH --output=FW_job-%j.out",
+            "#SBATCH --error=FW_job-%j.error",
+            "#SBATCH --ntasks=32",
+            "",
+            "module load ms/qchem/6.2.1",
+            "",
+            f"qchem {name}.qin {name}.qout"
+        ]
+    
     return "\n".join(lines)
 
 
@@ -108,11 +130,22 @@ smx = {
 }
 
 geom_opt = {
-   "maxiter":200,
+   "maxiter": 200,
    "coordinates": "redundant",
    "max_displacement": 0.1,
    "optimization_restart": "false",
 }
 
 if __name__ == "__main__":
-    main()
+    # Get what system we are using from the command line
+
+    if len(sys.argv) == 2:
+        system = sys.argv[1]
+    else:
+        raise Exception(
+            "Please provide a single command line argument with the name of the system.\nImplemnted for: {lrc, swift}"
+            )
+    if system not in ["lrc", "swift"]:
+        raise Exception("Please provide a valid system name: {lrc, swift}")
+    
+    main(system)
